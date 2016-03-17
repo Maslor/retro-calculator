1853131dac48758105925c34f36e2d382e619494
Maslor <gabriel.freire@tecnico.ulisboa.pt>
2016-03-17 16:58:52 +0000

    first commit

--- retro-calculator/ViewController.swift
+++ retro-calculator/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Sum = "+"
+        case Multiply = "*"
+        case Equals = "="
+        case Empty = "Empty"
+    }
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    var buttonSound: AVAudioPlayer!
+    
+    var displayNumber = ""
+    var leftArgString = ""
+    var rightArgString = ""
+    var currentOperator: Operation = Operation.Empty
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundURL = NSURL(fileURLWithPath: path!)
+        do {
+            try buttonSound = AVAudioPlayer(contentsOfURL: soundURL)
+            buttonSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func numberPressed(button: UIButton!) {
+        buttonSound.play()
+        displayNumber += "\(button.tag)"
+        outputLabel.text = displayNumber
     }
+    
 
-
+    @IBAction func onEqualPress(sender: AnyObject) {
+    }
+    @IBAction func onSumPress(sender: AnyObject) {
+    }
+    @IBAction func onSubtractPress(sender: AnyObject) {
+    }
+    @IBAction func onDividePress(sender: AnyObject) {
+    }
+    @IBAction func onMultiplyPress(sender: AnyObject) {
+    }
 }
 
